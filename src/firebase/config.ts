ec89a7aff2bfe46a8aacc5065023f71716e9846d
Renato Mazzarino <renatomazzarinocorp@gmail.com>
2025-10-29 23:29:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6201763510-1a97b",
+  "appId": "1:536768004337:web:af1360e13f3f36a7ffe950",
+  "apiKey": "AIzaSyBDxEmDdGsw2o3kxfW0MKqIz6C3x2bIYxU",
+  "authDomain": "studio-6201763510-1a97b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "536768004337"
+};
