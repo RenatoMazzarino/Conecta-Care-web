c0c7bd806063a885138be1e184c4c8251a5037a4
Renato Mazzarino <renatomazzarinocorp@gmail.com>
2025-10-31 00:54:59 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,16 +5,6 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
-// Load configuration from environment variables
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
 type FirebaseServices = {
   firebaseApp: FirebaseApp;
   auth: Auth;
@@ -43,10 +33,20 @@ export function initializeFirebase(): FirebaseServices {
       firestore: getFirestore(app)
     };
   }
+  
+  // Load configuration from environment variables INSIDE the function
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  };
 
+  // Use dynamic hostname on the client to solve auth domain issues in dev environments.
   const clientConfig = {
     ...firebaseConfig,
-    // Use dynamic hostname on the client to solve auth domain issues in dev environments.
     authDomain: window.location.hostname,
   };
 
